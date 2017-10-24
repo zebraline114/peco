f4e4fcb343c4d75ffc7e84cc01e5c1f09197dfda
zebraline114 <jessikrapp@gmx.de>
2017-10-24 22:55:04 +0200

    Distanzsensor mit Voltausgabe (Kommentare aufgeräumt) Servomotor: Bewegt sich via Schleife im 180 Grad Radius

--- SW/GP2Y0A21YK0F_DistSensor/GP2Y0A21YK0F_DistSensor.ino
+++ SW/GP2Y0A21YK0F_DistSensor/GP2Y0A21YK0F_DistSensor.ino
@@ -9,15 +9,10 @@ void loop() {
   
   int iAIdistSensor = analogRead(iDistanceSensor);  //Read analog in Value
   float fVoltage = (float)iAIdistSensor * 0.0048828125f;
-//  int iPercent = map(iAIdistSensor, 0, 500, 80, 10); //Convert Analog Value into percentage
-//  Serial.print("Distance Reading: ");
   Serial.print("Raw value ");
   Serial.print(iAIdistSensor);      //Print raw analog value
   Serial.print("    Voltage:  ");
   Serial.print(fVoltage);
   Serial.println("V");
-//  Serial.print(" Centimeters ");
-//  Serial.print(iPercent);         //Print distance
-//  Serial.println("cm");              //Print centimeter abbrev
   delay(1000);
 }
