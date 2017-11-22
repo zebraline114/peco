076a7b5c4515fa54b2ef1a07ab5786c71ba06dc4
zebraline114 <jessikrapp@gmx.de>
2017-11-22 21:18:07 +0100

    MOD:  - Neigungssensor TestIBN  - Messdaten Fahrwer auf Distanz in Excel

--- SW/Peco/src/Neigungssensor/Neigungssensor.ino
+++ SW/Peco/src/Neigungssensor/Neigungssensor.ino
@@ -1,11 +1,16 @@
-void setup() {
 
-  pinMode(S9, INPUT);
-  pinMode(S10, INPUT); 
   boolean nachOben;
   boolean nachUnten;
-  boolean nachRecht;
+  boolean nachRechts;
   boolean nachLinks;  
+
+void setup() {
+
+  Serial.begin(9600); 
+
+  pinMode(9, INPUT);
+  pinMode(10, INPUT); 
+
 
 }
 
@@ -17,12 +22,13 @@ void loop() {
 
 void aktuelleRichtung() {
 
-  int pin9 = 0;
+ /* int pin9 = 0;
   int pin10 = 0;
+  
 
   nachOben = false;
   nachUnten = false;
-  nachRecht = false;
+  nachRechts = false;
   nachLinks = false;
 
   if (digitalRead(9) == HIGH) {
@@ -36,24 +42,39 @@ void aktuelleRichtung() {
     pin10 = 1;
   }
   else {
-    pin10 = 1;
+    pin10 = 0;
   }
 
   if (pin9 == 1 && pin10 == 1) {
     nachRechts = true;
   }
 
-  if (pin9 == 1 && pin10 = 0) {
+  if (pin9 == 1 && pin10 == 0) {
     nachUnten = true;
   }
 
-  if (pin9 == 0 && pin10 = 1) {
+  if (pin9 == 0 && pin10 == 1) {
     nachRechts = true;
   }
 
-  if (pin9 == 0 && pin10 = 0) {
+  if (pin9 == 0 && pin10 == 0) {
     nachOben = true;
   }
+
+     /*   Serial.print("nachRechts: ");
+        Serial.print(nachRechts);
+        Serial.print("  nachLinks: ");
+        Serial.print(nachLinks);
+        Serial.print("  nachOben: ");
+        Serial.print(nachOben);
+        Serial.print("  nachUnten: ");
+        Serial.println(nachUnten);*/
+        Serial.print("  PIN9: ");
+                Serial.print(digitalRead(9));
+        Serial.print("  PIN10: ");
+                Serial.println(digitalRead(10));
+
+        delay(250);
   
   
 }
